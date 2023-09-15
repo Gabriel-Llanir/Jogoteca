9edc98cdb14d45cc092fcb22d0fde9035bdb0461
Gabriel Llanir <104911512+Gabriel-Llanir@users.noreply.github.com>
2023-09-15 00:42:23 -0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
         SGBD='mysql+mysqlconnector',
         usuario='root',
-        senha='InsaneXurow10',
+        senha='root',
         servidor='localhost',
         database='jogoteca'
     )
